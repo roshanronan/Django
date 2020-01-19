fd8cb11dbb32fd9dec5aaa6ad1a161f9f4da890e
roshanronan <singhroshandu@gmail.com>
2020-01-20 00:51:58 +0530

    costmize admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Listing
 
-# Register your models here.
+class ListingAdmin(admin.ModelAdmin):
+    list_display =('id','title','is_published','price','list_date','realtor')
+    list_display_links=('id','title')
+    list_filter=('realtor',)
+    list_editable=('is_published',)
+    search_fields=('id','title','price','zipcode','address','city','state','description')
+    list_per_page=10
+admin.site.register(Listing, ListingAdmin)
