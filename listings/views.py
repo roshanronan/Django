0a7bc7cec2f68c4d3d81580ff9c163ea57af7c93
roshanronan <singhroshandu@gmail.com>
2020-01-25 00:27:24 +0530

    added search funtionality

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404,render
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 from .models import Listing
+from .choices  import state_choices, bedroom_choices, price_choices
 
 # Create your views here.
 def index(request):
@@ -23,4 +24,46 @@ def listing(request,listing_id):
     return render(request,'listings/listing.html',context)
 
 def search(request):
-    return render(request,'listings/search.html')
+    query_list = Listing.objects.order_by('-list_date')
+
+    #Search Fields
+
+    #keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_list=query_list.filter(description__icontains=keywords)
+            
+    #city
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_list=query_list.filter(city__iexact=city)
+
+    #state
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_list=query_list.filter(state__iexact=state)
+
+    #bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_list=query_list.filter(bedrooms__lte=bedrooms)
+
+    #price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_list=query_list.filter(price__lte=price) #lte means it will look less or equal to price value
+
+
+    context = {
+        'bedroom_choices':bedroom_choices,
+        'state_choices':state_choices,
+        'price_choices':price_choices,
+        'listings': query_list,
+        'values':request.GET
+    }
+    return render(request, 'listings/search.html',context)
